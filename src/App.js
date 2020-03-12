ed92b4af8dd5cbe0aadb687e0020606bd6e8fe54
pdrobyshev <pdrobyshev@wooppay.com>
2020-03-12 17:53:21 +0600

    Added map to dynamically create components Added remove component function in immutable way

--- src/App.js
+++ src/App.js
@@ -9,17 +9,8 @@ class App extends Component {
             {name: 'Manu', age: 29},
             {name: 'Stephanie', age: 26}
         ],
-        otherState: 'some other value'
-    };
-
-    switchNameHandler = (newName) => {
-        this.setState({
-            persons: [
-                {name: newName, age: 28},
-                {name: 'Manu', age: 29},
-                {name: 'Stephanie', age: 27}
-            ]
-        });
+        otherState: 'some other value',
+        showPersons: false
     };
 
     nameChangedHandler = (event) => {
@@ -32,6 +23,18 @@ class App extends Component {
         });
     };
 
+    deletePersonHandler = (personIndex) => {
+        // const persons = this.state.persons.slice();
+        const persons = [...this.state.persons];
+        persons.splice(personIndex, 1);
+        this.setState({persons: persons});
+    };
+
+    togglePersonsHandler = () => {
+        const doesShow = this.state.showPersons;
+        this.setState({showPersons: !doesShow});
+    };
+
     render() {
         const style = {
             backgroundColor: 'white',
@@ -41,28 +44,35 @@ class App extends Component {
             cursor: 'pointer'
         };
 
+        let persons = null;
+
+        if (this.state.showPersons) {
+            persons = (
+                <div>
+                    {
+                        this.state.persons.map((person, index) => {
+                            return <Person
+                                click={() => this.deletePersonHandler(index)}
+                                name={person.name}
+                                age={person.age}/>
+                        })
+                    }
+                </div>
+            );
+        }
+
         return (
             <div className="App">
                 <h1>Hi, I'm a React App</h1>
                 <p>This is really working!</p>
+
                 <button
                     style={style}
-                    onClick={() => this.switchNameHandler('Maximilian!!')}>
-                    Switch Name
+                    onClick={this.togglePersonsHandler}>
+                    Toggle persons
                 </button>
-                <Person
-                    name={this.state.persons[0].name}
-                    age={this.state.persons[0].age}/>
-                <Person
-                    name={this.state.persons[1].name}
-                    age={this.state.persons[1].age}
-                    click={this.switchNameHandler.bind(this, 'Max!')}
-                    changed={this.nameChangedHandler}>
-                    My Hobbies: Racing
-                </Person>
-                <Person
-                    name={this.state.persons[2].name}
-                    age={this.state.persons[2].age}/>
+
+                {persons}
             </div>
         );
         // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
