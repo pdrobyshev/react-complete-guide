7c42d7971ab1208de3173b0a565390a242d8bf90
Pyotr Drobyshev <pa.drobyshev@gmail.com>
2020-04-11 17:34:58 +0600

    added appTitle as props to main app component

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 
-render(<App/>, document.getElementById('root'));
+render(<App appTitle="Person Manager"/>, document.getElementById('root'));
 registerServiceWorker();
